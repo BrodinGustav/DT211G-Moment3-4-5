69fedf4f09277dcf121c7db5811b5db00bc83c0c
Gustav Brodin <brodin.gustav@gmail.com>
2024-02-22 11:47:04 +0100

    lagt till gitignore-fil

---
+++ dist/index.d3e90fdb.js
@@ -0,0 +1,2 @@
+let e=document.getElementById("open-menu"),n=document.getElementById("close-menu");function t(){let e=document.getElementById("nav-menu");"none"===window.getComputedStyle(e).display?e.style.display="block":e.style.display="none"}e.addEventListener("click",t),n.addEventListener("click",t);
+//# sourceMappingURL=index.d3e90fdb.js.map
